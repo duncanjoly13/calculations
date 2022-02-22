dfd31e62bf0180e18219d1bf08b564448c2e77da
Duncan Joly <duncanjoly13@gmail.com>
2022-02-22 15:49:52 -0600

    Update pi.py
    Increased speed and updated output

--- pi.py
+++ pi.py
@@ -1,8 +1,8 @@
 from math import *
-#import numpy as np
+# import numpy as np
 
-n= 200000000
-x= 1
+n = 2400000000
+x = 1
 series = []
 
 def pi_approx(num_iterations):
@@ -12,9 +12,9 @@ def pi_approx(num_iterations):
     for i in range(num_iterations):
         s = s-((1/k) * (-1)**i)
         k += 2
-        if i%10000 == 0:
-            print(4*s)
-
+        if i%100000 == 0:
+            print(str(i)+"th calculation: "+str(4*s))
+            
     return 4 * s
 
 print(pi_approx(n))

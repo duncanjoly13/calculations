d6fc811e9d44ba1d11a4dfd62b2cb96b1d703fc2
Duncan Joly <duncanjoly13@gmail.com>
2022-03-01 15:29:55 -0600

    added progress bar option and comments
    removed unused lines
    new dependencies: alive_progress
    cited sources for Taylor series code

--- pi.py
+++ pi.py
@@ -1,19 +1,51 @@
+# authors: Duncan Joly, Kieran Musser
+# some code from user4511836 and Vincent on stackoverflow
+# date: 2/22/2022
+# requirements: alive_progress
+
+# import modules
+import time
 from math import *
+from alive_progress import alive_bar
 
+# set number of iterations
 n = 2400000000
-x = 1
-series = []
 
-def pi_approx(num_iterations):
+"""define approximation function
+params:
+    int num_iterations is number of iterations
+    boolean progressBar should be set to True in order to display a progress bar
+        if progressBar is false it will print out updates every 10th of a percent of total progress"""
+def pi_approx(num_iterations, progressBar):
+    # unsure what these do - from stackexchange
     k = 3.0
     s = 1.0
 
-    for i in range(num_iterations):
-        s = s-((1/k) * (-1)**i)
-        k += 2
-        if i%(n/1000) == 0:
-            print(str(i*100/n)+" percent done: "+str(4*s))
+    # check if should display progress bar
+    if progressBar:
+        # progress bar parameters - total length is n/100
+        with alive_bar(24000000, spinner='fish2', title="completion") as bar:
+            # loop to calculate Taylor series n times
+            for i in range(num_iterations):
+                s = s-((1/k) * (-1)**i)
+                k += 2
+                # updates progress bar
+                if i%(n/100) == 0:
+                    bar()
+        # final return value
+        return 4 * s
 
-    return 4 * s
+    # if no progress bar
+    else:
+        # loop to calculate Taylor series n times
+        for i in range(num_iterations):
+            s = s-((1/k) * (-1)**i)
+            k += 2
+            if i%(n/1000) == 0:
+                # print updates every 10th of a percent
+                print(str(i*100/n)+" percent done: "+str(4*s))
+        # final return value
+        return 4 * s
 
-print(pi_approx(n))
+# demo
+print(pi_approx(n, False))
